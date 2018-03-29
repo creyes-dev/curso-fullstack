7c077ebefb4011d87b5d2d64608b50f3d6d175ff
creyes-dev <creyes.dev@gmail.com>
2018-03-28 21:29:08 -0300

    evaluacion

---
+++ 3_AJAX/10_evaluacion/cristianReyes.php
@@ -0,0 +1,22 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new mysqli("localhost", "root", "", "personas");
+
+$result = $conn->query("SELECT id_persona, nombre FROM `personas` WHERE 1");
+
+$outp = "[";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "[") {$outp .= ",";}
+    $outp .= '{"Id":"'  . $rs["id_persona"] . '",';
+    $outp .= '"Nombre":"'   . $rs["nombre"] . '"}';
+}
+$outp .="]";
+
+$conn->close();
+
+echo($outp);
+
+?>
