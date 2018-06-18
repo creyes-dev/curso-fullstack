fd2a915581083a623346bb7413776430b523e223
creyes-dev <creyes.dev@gmail.com>
2018-06-18 17:37:58 -0300

    listado de productos

--- 5_NodeJS/30_carroCompras/carro-compras/models/producto.js
+++ 5_NodeJS/30_carroCompras/carro-compras/models/producto.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
+    titulo: {type: String, required: true},
     imagen: {type: String, required: true},
     descripcion: {type: String, required :true},
     precio: {type: Number, required: true}
