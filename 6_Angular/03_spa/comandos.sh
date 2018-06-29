7e547afb704f66e0aec3c3f46a41e3fbca8bec9b
creyes-dev <creyes.dev@gmail.com>
2018-06-29 01:00:29 -0300

    single page application

---
+++ 6_Angular/03_spa/comandos.sh
@@ -0,0 +1,6 @@
+ng new spa
+cd spa
+ng generate component components/shared/navbar
+npm install bootstrap --save
+npm install jquery --save
+npm install popper.js --save
---
+++ 6_Angular/03_spa/spa/comandos.sh
@@ -0,0 +1,9 @@
+ng new spa
+cd spa
+ng generate component components/shared/navbar
+npm install bootstrap --save
+npm install jquery --save
+npm install popper.js --save
+ng generate component components/home
+ng generate component components/about
+ng generate component components/heroes
