a3fd7f004c95b691ec15af0205759932764ee205
creyes-dev <creyes.dev@gmail.com>
2018-07-22 23:56:21 -0300

    formulario heroes

---
+++ 6_Angular/08_formularios/comandos.sh
@@ -0,0 +1,13 @@
+ng new formularios
+cd formularios
+ng generate class hero
+ng generate component HeroForm
+
+npm install jquery --save
+npm install popper.js --save
+npm install bootstrap --save
+npm install tether --save
+
+cd modulos
+ng generate module nombre
+ng generate component nombre/miComponente
